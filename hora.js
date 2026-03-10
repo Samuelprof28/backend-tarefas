57155daf3886ffbb45b2163f9045d4810ed7091b
Samuelprof28 <samuel.pereira.silva28@escola.pr.gov.br>
2026-03-10 20:32:05 -0300

    versao1

--- hora.js
+++ hora.js
@@ -1,1 +1,6 @@
-//Valores iniciais do dia 
+// Valores iniciais do dia
+let valorPorHora = 50; 
+let horasTrabalhadas = 8; // Expediente padrão
+
+console.log("--- Resumo do Expediente ---");
+console.log(`Horas iniciais: ${horasTrabalhadas}h`);
