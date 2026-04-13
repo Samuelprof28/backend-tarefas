a22deecaf9c605357627d8011d307a5da8246903
Samuelprof28 <samuel.pereira.silva28@escola.pr.gov.br>
2026-04-13 11:50:08 +0000

    v1

---
+++ array1.js
@@ -0,0 +1,13 @@
+//Lista de frutas - ARRAY
+
+const frutas = ["Maçã", "Banana", "Uva", "Manga"];
+//ÍNDICE frutas    0        1       2       3
+
+console.log(frutas[1]);
+console.log(frutas[3]);
+console.log(frutas.length);
+frutas[1] = "Morango"; //Altera o valor do elemento
+frutas.push("Pêssego"); //Adiciona um item elemento no final
+frutas.unshift("Pêra");
+console.log(frutas);
+console.log(frutas.length);
